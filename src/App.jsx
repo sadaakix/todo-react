53af963b8122ee50995f7fd3244e6d1a7339b32b
sadaakix <100124850+sadaakix@users.noreply.github.com>
2022-06-30 17:04:15 +0900

    Props(コンポーネントに渡す引数のようなもの)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import ColorfulMessage from "./component/ColofulMessage";
 
 const App = () => {
   const onClickButton = () => alert("alert");
-  const contentStyle = {
-    color: "blue",
-    fontSize: "18px"
-  };
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <p style={contentStyle}>お元気ですか？</p>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onCLick={onClickButton}>ボタン</button>
     </>
   );
